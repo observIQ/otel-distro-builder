4a2e1c92f333ab75c6e57b1153ae791a22dd51f9
Michael Kelly <michael.e.kelly@gmail.com>
2025-04-04 00:33:22 -0400

    lint

--- builder/tests/test_version.py
+++ builder/tests/test_version.py
@@ -2,12 +2,9 @@
 
 import pytest
 import yaml
-from src.version import (
-    MIN_SUPERVISOR_VERSION,
-    BuildVersions,
-    determine_build_versions,
-    get_contrib_version_from_manifest,
-)
+from src.version import (MIN_SUPERVISOR_VERSION, BuildVersions,
+                         determine_build_versions,
+                         get_contrib_version_from_manifest)
 
 
 @pytest.mark.base
